6c116fc896a5c9ba398f34cde4a06c139c4fa24e
Tooma81 <toomas.kaarel@gmail.com>
2026-05-18 11:48:46 +0300

    Change name to subject field

--- send_form.php
+++ send_form.php
@@ -1,11 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
+    $subject = $_POST["subject"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
     $to = "toomas-kaarel.elmet@voco.ee";
-    $subject = "E-portfolio Contact Form Submission";
     $headers = "From: " . $email;
 
     mail($to, $subject, $message, $headers);
